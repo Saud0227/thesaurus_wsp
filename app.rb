4edd3dbf8975933745150418dd99d0f79240b7a3
Saud0227 <Saud02Tech@gmail.com>
2023-09-28 14:06:43 +0200

    Work during lesson
    Co-authored-by: TurBin05 <tural.binnatov@elev.ga.ntig.se>

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
+require 'csv'
 
 class App < Sinatra::Base
+
+  @@allowed_word_types = ['nouns', 'verbs', 'adverbs', 'adjectives']
+
   get '/' do
-    'Hello world!'
+    @title = "Hello World"
+    erb :index
+  end
+
+  get '/words' do
+    @title = "Words"
+    erb :words
+  end
+
+  get '/words/:type' do |word_type|
+    unless @@allowed_word_types.include?(word_type)
+      redirect '/words'
+    end
+
+    rows = CSV.read("/public/data/#{word_type.capitalize}.csv")
+
+    @title = word_type.capitalize
+    @word_type = word_type
+    @words = rows
+
+    puts rows
+    erb :show_words
   end
 end
